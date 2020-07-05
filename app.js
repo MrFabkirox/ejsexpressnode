20bff5b85630457869921a8d8e0c5437388b1b4c
mrfabkirox <fabrice.esope@gmail.com>
2020-07-05 11:42:50 +0800

    delete broken

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const methodOverride = require('method-override');
 
 const mongoose = require('mongoose');
 require('dotenv').config()
 
 const uri = process.env.ATLAS_URI
 mongoose.connect(uri, {
-    dbname: 'tigernodesandreact',
+//    dbname: 'tigernodesandreact',
     useNewUrlParser: true,
     useUnifiedTopology: true
   }
@@ -17,10 +18,13 @@ connection.once('open', () => {
     console.log("MongoDB atlas connected");
 })
 
+app.use(express.urlencoded({ extended: false }))
+
 // 1
 app.use('/public', express.static(path.join(__dirname, 'static')))
 app.set('view engine', 'ejs');
 
+app.use(methodOverride('_method'))
 const Item = require('./models/Item');
 
 app.get('/items', (req, res) => {
@@ -28,6 +32,38 @@ app.get('/items', (req, res) => {
     .then(items => res.render('index', { items } ))
     .catch(err => res.status(404).json({ msg: 'err' } ))
 });
+
+app.post('/item', async (req, res) => {
+  let item = new Item({
+    name: req.body.name,
+  });
+  try {
+    item = await item
+      .save()
+      .then(result => {
+        console.log('item saved [%o]', item);
+        res.json({
+          item: "some"
+        });
+      });
+  } catch (err) {
+    console.log('_______item [%o]', item);
+    console.log('_______error [%o]', err);
+  }
+});
+
+app.delete('/', async (req, res) => {
+  const id = req.params.id;
+  Item.remove({ _id: _id })
+    .exec()
+    .then(result => {
+       res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log('_______error [%o]', err);
+    });
+});
+
 
 // // 1 2
 // app.get('/:userQuery', (req,res) => {
